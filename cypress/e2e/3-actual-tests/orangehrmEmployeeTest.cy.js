4258aadecc4bacc41f95fb39ae6a2fdda170e2a0
unknown <gula_roby2@yahoo.com>
2025-07-22 15:42:12 +0300

    Delete users

--- cypress/e2e/3-actual-tests/orangehrmEmployeeTest.cy.js
+++ cypress/e2e/3-actual-tests/orangehrmEmployeeTest.cy.js
@@ -24,7 +24,7 @@ describe('OrangeHRM Employee Tests', () => {
 
         // Add assertions to verify the employee was added successfully
     });
-    it.only('should search and select an employee', () => {
+    it('should search and select an employee', () => {
         OrangeLoginPage.visit();
         OrangeLoginPage.fillUsername('Admin');
         OrangeLoginPage.fillPassword('admin123');
@@ -36,5 +36,23 @@ describe('OrangeHRM Employee Tests', () => {
         cy.get('.oxd-form-actions > .oxd-button--secondary').click();
 
         // Add assertions to verify the employee was selected successfully
+    });
+    it("should delete all users it finds", () => {
+        OrangeLoginPage.visit();
+        OrangeLoginPage.fillUsername('Admin');
+        OrangeLoginPage.fillPassword('admin123');
+        OrangeLoginPage.submit(); 
+        cy.wait(4000); // Wait for the page to load
+        OrangeEmployeeADD.visit();
+        cy.wait(2000); // Wait for the page to load
+        OrangeEmployeeADD.searchAndSelectEmployee('Ranga', 'Ranga');
+        cy.get('.oxd-form-actions > .oxd-button--secondary').click();
+        cy.get('.oxd-table-cell-actions > :nth-child(1)').each(($btn, index, $list) => {
+  cy.wrap($btn).click();
+  cy.get('.oxd-button--label-danger').click();
+  cy.wait(4000); // Wait after each delete
+});
+
+        // Add assertions to verify the employee was deleted successfully
     });
 });
